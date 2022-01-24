30f0570702e44a5a65eccb53608102744bf2a70d
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-24 14:28:43 -0800

    got my app test working

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,16 @@
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import App from './App'
 
-test('Should render the header', () => {
+test('Should render the header', async () => {
+  render(<App />)
 
+  const heading = screen.getByText(/meet/i)
+
+  const name = await screen.findByText(/vonta/i)
+
+  const logo = await screen.findByAltText(/alchemy logo/i)
+
+  expect(heading).toBeInTheDocument()
+  expect(name).toBeInTheDocument()
+  expect(logo).toBeInTheDocument()
 })
